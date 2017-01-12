2f56598202ca49877854c9fa7e92bf5467c14fa6
austinkelleher <austin.kell47@gmail.com>
2017-01-12 13:19:50 -0500

    Additional tests.

--- README.md
+++ README.md
@@ -7,12 +7,18 @@ Machine specs: MacBook Pro (2.5 GHz Intel Core i7, 16 GB 1600 MHz DDR3) running
 OS X El Capitan and Node.js v7.2.1
 
 ```
-Object#create prototype x 96,441,130 ops/sec ±1.65% (82 runs sampled)
-Object#create prototype hit x 87,560,346 ops/sec ±1.67% (85 runs sampled)
-Object#create prototype miss x 88,179,069 ops/sec ±1.75% (83 runs sampled)
-Object#create null x 19,123,828 ops/sec ±1.26% (84 runs sampled)
-Object#create null hit x 88,030,576 ops/sec ±1.41% (88 runs sampled)
-Object#create null miss x 88,126,215 ops/sec ±1.40% (86 runs sampled)
+Object#create prototype x 96,121,992 ops/sec ±1.53% (83 runs sampled)
+Object#create prototype hit x 88,656,642 ops/sec ±1.69% (85 runs sampled)
+Object#create prototype miss x 88,448,656 ops/sec ±1.68% (85 runs sampled)
+Object#create prototype create delete __proto__ x 17,091,139 ops/sec ±1.30% (88 runs sampled)
+Object#create prototype delete __proto__ with keys x 18,337,124 ops/sec ±0.91% (86 runs sampled)
+Object#create prototype delete __proto__ without keys x 19,515,279 ops/sec ±0.94% (85 runs sampled)
+Object#create new x 30,436,062 ops/sec ±1.01% (88 runs sampled)
+Object#create new hit x 90,577,878 ops/sec ±1.69% (84 runs sampled)
+Object#create new miss x 90,737,806 ops/sec ±1.53% (83 runs sampled)
+Object#create null x 18,791,021 ops/sec ±1.12% (87 runs sampled)
+Object#create null hit x 92,049,928 ops/sec ±1.46% (85 runs sampled)
+Object#create null miss x 89,745,354 ops/sec ±1.61% (82 runs sampled)
 ```
 
 ## Running
--- benchmark.js
+++ benchmark.js
@@ -5,29 +5,54 @@ let objProto = {};
 objProto.a = 'b';
 objProto.c = 5;
 
+let objProto2 = {};
+
+let objNew = new Object();
+objNew.a = 'b';
+objNew.c = 5;
+
 let objNullProto = Object.create(null);
 objNullProto.a = 'b';
 objNullProto.c = 5;
 
-suite.add('Object#create prototype', function() {
+suite.add('Object#create prototype', function () {
     {};
 })
-.add('Object#create prototype hit', function() {
+.add('Object#create prototype hit', function () {
     objProto.a;
 })
-.add('Object#create prototype miss', function() {
+.add('Object#create prototype miss', function () {
     objProto.b;
 })
-.add('Object#create null', function() {
+.add('Object#create prototype create delete __proto__', function() {
+    let objProto = {};
+    delete objProto.__proto__;
+})
+.add('Object#create prototype delete __proto__ with keys', function() {
+    delete objProto.__proto__;
+})
+.add('Object#create prototype delete __proto__ without keys', function() {
+    delete objProto2.__proto__;
+})
+.add('Object#create new', function () {
+    new Object();
+})
+.add('Object#create new hit', function () {
+    objNew.a;
+})
+.add('Object#create new miss', function () {
+    objNew.b;
+})
+.add('Object#create null', function () {
     Object.create(null);
 })
-.add('Object#create null hit', function() {
+.add('Object#create null hit', function () {
     objNullProto.a;
 })
-.add('Object#create null miss', function() {
+.add('Object#create null miss', function () {
     objNullProto.b;
 })
-.on('cycle', function(event) {
+.on('cycle', function (event) {
   console.log(String(event.target));
 })
 .on('complete', function () {
